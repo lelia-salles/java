e21c459605fdc740bcf2ad5419c8ec31b21ff83c
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-09 02:57:46 +0000

    added cases

---
+++ Controle-de-Fluxo/EstruturasExcepcionais/TryCatch.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close(); 
+        
+        try {
+            //  bloco de código conforme esperado
+          }
+          catch(Exception e) {// precisamos saber qual exceção
+            // bloco de código que captura as exceções que podem acontecer
+            // em caso de um fluxo não previsto
+          }  
+    }
+}
