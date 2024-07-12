ca3188e86d204fcb65557c2b7d1973b1da222a91
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-12 23:09:53 +0000

    added getters and setter

--- Getter_Setters/escola/src/escola/escola/Escola.java
+++ Getter_Setters/escola/src/escola/escola/Escola.java
@@ -1,11 +1,10 @@
 package escola;
 public class Escola {
-    public static void main(String[] args) {
-        Aluno felipe = new Aluno(); // cria objeto
-        felipe.nome = "Felipe"; // define a estrutura
-        felipe.idade = 8;	
-        System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
-
-    }
-
+	public static void main(String[] args) {
+		Aluno felipe = new Aluno();
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
+		
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");	
+	}
 }
