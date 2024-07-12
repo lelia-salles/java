df4d641f274eec25db960408320f1dffa963db5f
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-12 22:58:07 +0000

    comments

--- Getter_Setters/escola/src/escola/escola/Escola.java
+++ Getter_Setters/escola/src/escola/escola/Escola.java
@@ -1,8 +1,8 @@
 package escola;
 public class Escola {
     public static void main(String[] args) {
-        Aluno felipe = new Aluno();
-        felipe.nome = "Felipe";
+        Aluno felipe = new Aluno(); // cria objeto
+        felipe.nome = "Felipe"; // define a estrutura
         felipe.idade = 8;	
         System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
 
