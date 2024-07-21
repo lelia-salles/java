ea432bd02f77a155c0baec60be1305f5691db368
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-21 15:50:43 -0300

    updated

--- ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Encapsulamento/MSNMessenger.java
+++ ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Encapsulamento/MSNMessenger.java
@@ -1,6 +1,8 @@
 public class MSNMessenger {
 	public void enviarMensagem() {
+		validarConectadoInternet();
 		System.out.println("Enviando mensagem");
+		salvarHistoricoMensagem();
 	}
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem");
