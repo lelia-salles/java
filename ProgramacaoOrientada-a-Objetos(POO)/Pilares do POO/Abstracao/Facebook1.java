54d4fb1422e85fc61fde840b217ad8dd710507de
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-29 11:32:44 -0300

    add

---
+++ ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Abstracao/Facebook1.java
@@ -0,0 +1,8 @@
+public class Facebook1 extends ServicoMensagemInstantanea1 {
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo Facebook Messenger");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Facebook Messenger");
+	}
+}
