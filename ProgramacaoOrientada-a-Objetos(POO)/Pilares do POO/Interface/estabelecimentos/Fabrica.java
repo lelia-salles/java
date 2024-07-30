e6ec067d717f7fcfd590845448e8edb2c8104e5c
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-30 10:36:15 -0300

    update

--- ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Interface/estabelecimentos/Fabrica.java
+++ ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Interface/estabelecimentos/Fabrica.java
@@ -4,16 +4,24 @@ import equipamentos.impressora.Deskjet;
 import equipamentos.impressora.Impressora;
 import equipamentos.impressora.Laserjet;
 import equipamentos.impressora.EquipamentoMultifuncional;
+import equipamentos.digitalizadora.Scanner;
+import equipamentos.digitalizadora.Digitalizadora;
 
 public class Fabrica {
     public static void main(String[] args) {
+
+        //reusabilidade 
         EquipamentoMultifuncional em = new EquipamentoMultifuncional();
         Deskjet deskjet = new Deskjet();
+        Scanner scanner = new Scanner();
         //Impressora impressora = new Deskjet();
         //Impressora impressora = new Laserjet();
         Impressora impressora = em;
+        Impressora impressora = scanner;
         Digitalizadora digitalizadora = em;
+        Digitalizadora digitalizadora = scanner;
         Copiadora copiadora = em;
+        Copiadora copiadora = scanner
 
 
 
