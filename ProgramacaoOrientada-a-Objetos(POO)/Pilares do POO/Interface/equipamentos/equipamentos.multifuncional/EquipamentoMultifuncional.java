f9f7dfc141096d0bd65d485b0bc26e8368c95479
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-30 10:31:54 -0300

    update

--- ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Interface/equipamentos/equipamentos.multifuncional/EquipamentoMultifuncional.java
+++ ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Interface/equipamentos/equipamentos.multifuncional/EquipamentoMultifuncional.java
@@ -1,5 +1,24 @@
 package equipamentos.multifuncional;
 
-public class EquipamentoMultifuncional {
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+
+    @Override
+    public void copiar(){
+        System.out.println('Copiando via equipamento multifuncional');
+
+    }
+
+    @Override
+    public void digitalizar(){
+        System.out.println('digitalizando via equipamento multifuncional');
+
+    }
+
+    @Override
+    public void imprimir(){
+        System.out.println('Imprimindo via equipamento multifuncional');
+
+    }
+
     
 }
--- ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Interface/estabelecimentos/Fabrica.java
+++ ProgramacaoOrientada-a-Objetos(POO)/Pilares do POO/Interface/estabelecimentos/Fabrica.java
@@ -1,7 +1,25 @@
 package estabelecimentos;
 
+import equipamentos.impressora.Deskjet;
+import equipamentos.impressora.Impressora;
+import equipamentos.impressora.Laserjet;
+import equipamentos.impressora.EquipamentoMultifuncional;
+
 public class Fabrica {
     public static void main(String[] args) {
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+        Deskjet deskjet = new Deskjet();
+        //Impressora impressora = new Deskjet();
+        //Impressora impressora = new Laserjet();
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+
+
+
+       impressora.imprimir();
+       digitalizadora.digitalizar();
+       copiadora.copiar();
 
     }
     }
