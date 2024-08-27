c335ac55ab07617b7a68e201e5690279ee346b22
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-08-27 15:13:55 +0000

    update

--- Springboot/primeiros_passos/src/main/java/dio/PrimeirosPassosApplication.java
+++ Springboot/primeiros_passos/src/main/java/dio/PrimeirosPassosApplication.java
@@ -1,4 +1,5 @@
-package dio.primeiros_passos
+package dio.primeiros_passos;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- Springboot/primeiros_passos/src/test/java/PrimeirosPassosApplicationTests.java
+++ Springboot/primeiros_passos/src/test/java/PrimeirosPassosApplicationTests.java
@@ -1,4 +1,4 @@
-package dio.springboot;
+package dio.primeiros_passos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
