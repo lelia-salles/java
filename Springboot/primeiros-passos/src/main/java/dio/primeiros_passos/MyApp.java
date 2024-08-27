ec7df8983d5f8edcc9b1e4338f36cda9a8eb0043
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2024-08-27 20:21:05 -0300

    update

--- Springboot/primeiros-passos/src/main/java/dio/primeiros_passos/MyApp.java
+++ Springboot/primeiros-passos/src/main/java/dio/primeiros_passos/MyApp.java
@@ -2,6 +2,7 @@ package dio.primeiros_passos;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /*
 * classe semelhante ao método main para implemtar o command Line Runner
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MyApp implements CommandLineRunner{
+    @Autowired // injeção de dependencia dos componentes
     private Calculadora calculadora; //precisa da calculadora
     @Override
     public void run(String... args) throws Exception {
