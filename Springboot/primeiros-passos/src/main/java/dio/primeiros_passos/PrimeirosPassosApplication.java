f48f52528e93035bb76092542d5263abf2f55687
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2024-08-27 18:33:48 -0300

    update

--- Springboot/primeiros-passos/src/main/java/dio/primeiros_passos/PrimeirosPassosApplication.java
+++ Springboot/primeiros-passos/src/main/java/dio/primeiros_passos/PrimeirosPassosApplication.java
@@ -2,12 +2,27 @@ package dio.primeiros_passos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
+
+		//cria o objeto scanner para ler a entrada do usuário
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite um número inteiro: ");
+		int num1 = scanner.nextInt();
+		System.out.println("Digite o segundo número inteiro: ");
+		int num2 = scanner.nextInt();
+
+		int soma = num1 + num2;
+
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
+		Calculadora calculadora = new Calculadora();
+
+		System.out.println("o resultado é: " + soma);
 	}
 
 }
