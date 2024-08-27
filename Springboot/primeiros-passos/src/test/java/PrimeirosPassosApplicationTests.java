4ee1f0d5a9d0207f4a7ed9bc199470069ab2c826
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-08-27 14:41:23 +0000

    update

---
+++ Springboot/primeiros-passos/src/test/java/PrimeirosPassosApplicationTests.java
@@ -0,0 +1,15 @@
+package dio;
+
+import java.beans.Transient;
+
+@SpringBootApplicationTest
+/**
+ * PrimeirosPassosApplicationTests
+ */
+public class PrimeirosPassosApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
