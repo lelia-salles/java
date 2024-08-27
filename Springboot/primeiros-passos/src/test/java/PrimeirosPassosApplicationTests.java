ebcbc25f216e9074a408419d32647c52b7a6a595
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-08-27 14:42:42 +0000

    update

--- Springboot/primeiros-passos/src/test/java/PrimeirosPassosApplicationTests.java
+++ Springboot/primeiros-passos/src/test/java/PrimeirosPassosApplicationTests.java
@@ -1,8 +1,10 @@
 package dio;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import java.beans.Transient;
 
-@SpringBootApplicationTest
+@SpringBootTest
 /**
  * PrimeirosPassosApplicationTests
  */
