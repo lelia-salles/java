42840f207669839697df568a3fa6563fa1501519
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-08-29 08:43:16 -0300

    Add files via upload

---
+++ Springboot/aula-spring-data-jpa/src/main/java/aula_spring_jpa/AulaSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package aula_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringDataJpaApplication.class, args);
+	}
+
+}
