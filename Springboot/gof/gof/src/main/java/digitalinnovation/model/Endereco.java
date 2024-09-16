6c55a992a575399c24291040e1410c8d60dfc55d
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-16 11:58:50 -0300

    update

--- Springboot/gof/gof/src/main/java/digitalinnovation/model/Endereco.java
+++ Springboot/gof/gof/src/main/java/digitalinnovation/model/Endereco.java
@@ -2,4 +2,33 @@ package digitalinnovation.model;
 
 public class Endereco {
 
+    @Id
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia; 
+    private String ddd;
+    private String siafi;
+
+    /**
+     * Retorna o cep do endereo
+     * @return o cep do endereo
+     */
+
+    public String getCep() {
+        return cep;
+}
+
+    /**
+     * Altera o cep do endereo
+     * @param cep o novo cep do endereo
+     */
+public void setCep(String cep) {
+        this.cep = cep;
+}   
+
 }
