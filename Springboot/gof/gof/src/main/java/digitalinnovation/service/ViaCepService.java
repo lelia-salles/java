3957aabc281a32db0ffdab8edf226e8c0a85a667
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-16 12:25:49 -0300

    update

--- Springboot/gof/gof/src/main/java/digitalinnovation/service/ViaCepService.java
+++ Springboot/gof/gof/src/main/java/digitalinnovation/service/ViaCepService.java
@@ -1,5 +1,13 @@
 package digitalinnovation.service;
 
+// client do Feign
+
+@FeignClient   (name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
+
+    RequestMapping(value = "/{cep}/json", method = RequestMethod.GET(value = "/{cep}/json"))
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
+    
 
 }
