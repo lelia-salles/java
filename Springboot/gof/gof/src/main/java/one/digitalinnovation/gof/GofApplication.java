c8bd1425326ca01f73ba5c2c7502d536d7b69fc7
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-16 11:39:31 -0300

    updagte

--- Springboot/gof/gof/src/main/java/one/digitalinnovation/gof/GofApplication.java
+++ Springboot/gof/gof/src/main/java/one/digitalinnovation/gof/GofApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GofApplication {
 
+	@EnableFeignClients
+	@EnableSpringBootApplication;
 	public static void main(String[] args) {
 		SpringApplication.run(GofApplication.class, args);
 	}
