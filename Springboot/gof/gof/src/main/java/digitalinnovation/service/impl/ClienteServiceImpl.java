23f5037f8776ef30d644428ce6ab710badc6e445
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-16 12:20:48 -0300

    update

--- Springboot/gof/gof/src/main/java/digitalinnovation/service/impl/ClienteServiceImpl.java
+++ Springboot/gof/gof/src/main/java/digitalinnovation/service/impl/ClienteServiceImpl.java
@@ -1,5 +1,36 @@
 package digitalinnovation.service.impl;
 
-public class ClienteServiceImpl {
+public class ClienteServiceImpl implements ClienteService {
 
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        // TODO Auto-generated method stub  
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+        // TODO Auto-generated method stub
+
+    }   
+
+    @Override
+    public void buscarPorId(Long id) {
+        // TODO Auto-generated method stub
+
+    }   
+
+    
 }
