4db6373cec36304acfb8966d114510f3290c7a1e
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-17 11:28:36 -0300

    update

--- Springboot/gof/gof/src/main/java/digitalinnovation/GofApplication.java
+++ Springboot/gof/gof/src/main/java/digitalinnovation/GofApplication.java
@@ -1,25 +1,17 @@
-package one.digitalinnovation.gof;
+package digitalinnovation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import one.digitalinnovation.gof.config.EnableSpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.function.ServerResponse;
-import org.springframework.web.util.UriComponentsBuilder;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableFeignClients
 @RestController
 @SpringBootApplication
 public class GofApplication {
 
-	@EnableFeignClients
-	@EnableSpringBootApplication;
 	public static void main(String[] args) {
 		SpringApplication.run(GofApplication.class, args);
 	}
