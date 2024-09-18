c4eda39d01723b2e7da7566ed86f3b6a8b5f993e
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-18 09:33:26 -0300

    update

--- Springboot/gof/gof/src/main/java/digitalinnovation/model/Cliente.java
+++ Springboot/gof/gof/src/main/java/digitalinnovation/model/Cliente.java
@@ -39,24 +39,24 @@ public class Cliente {
     @JoinColumn(name = "id_endereco")
     private Endereco endereco;
 
-   public Long getId() {
-       return id;
-   }
-   public void setId(Long id) {
-       this.id = id;
-   }
-   public String getNome() {
-       return nome; 
-   } 
-   public void setNome(String nome) {
-       this.nome = nome;
-   }
-   public Endereco getEndereco() {
-       return endereco;
-   }
-   public void setEndereco(Endereco endereco) {
-       this.endereco = endereco;
-   }    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
 
 
