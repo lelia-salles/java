36e568c03678d01cc7996590bf49183ba2fc8c63
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-18 09:46:31 -0300

    update

--- Springboot/gof/gof/src/main/java/digitalinnovation/model/Cliente.java
+++ Springboot/gof/gof/src/main/java/digitalinnovation/model/Cliente.java
@@ -1,42 +1,19 @@
 package digitalinnovation.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManytoOne;
-import javax.persistence.Table;
-import org.hibernate.annotations.GenerateValue;
-import org.springframework.stereotype.Repository;
-import digitalinnovation.model.Endereco;
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.io.Serializable;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import javax.annotation.Generated;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import org.springframework.stereotype.Repository;
-import digitalinnovation.model.Endereco;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 
 
 @Entity
-@Table(name = "cliente")
-
-
-@Repository
 public class Cliente {
 
     @Id
-    @GenerateValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
-
     @ManytoOne
-    @JoinColumn(name = "id_endereco")
     private Endereco endereco;
 
     public Long getId() {
@@ -57,7 +34,4 @@ public class Cliente {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
-
-
-
 }
